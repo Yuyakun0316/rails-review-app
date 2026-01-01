01b5420be85dd4150330c049d52a71cd0a5f1be3
Yuyakun0316 <3.16.yuyakun@gmail.com>
2026-01-02 00:26:50 +0900

    annotaterbを導入：Rails 8対応版のアノテーションツールでスキーマ情報を追記

--- Gemfile
+++ Gemfile
@@ -72,6 +72,7 @@ group :development do
   gem 'rubocop', require: false
   gem 'rubocop-rails', require: false
   gem 'rubocop-rspec', require: false
+  gem 'annotaterb'
 end
 
 group :test do
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,5 +1,23 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :bigint           not null, primary key
+#  content    :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  post_id    :bigint           not null
+#
+# Indexes
+#
+#  index_comments_on_post_id  (post_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (post_id => posts.id)
+#
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
